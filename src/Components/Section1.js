1b3f97cb58f04d12bb30e70f5bbf7c0b9f03b648
M-Zayn-Abbas <156469257+M-Zayn-Abbas@users.noreply.github.com>
2024-02-19 14:06:48 +0500

    19/2 2:06pm

--- src/Components/Section1.js
+++ src/Components/Section1.js
@@ -1,12 +1,20 @@
+import pic from '../img/section1.jpg'
+import '../Cssfiles/Section1.css';
 const Section1 = () => {
 
     return (
         <>
-        <section>
-    <h1>Little LEmonm</h1>
-    <p>lorem</p>
+        <section className="section1">
+            <div className='text-content'>
+    <h1>Little Lemon</h1>
+    <h2>Chicago</h2>
+        <h4 className='multiline'>We are a family owned Mediterranean restaurant,
+        focused on traditional recipies served with a modern
+        twist </h4>
+
     <button>Reseve Table</button>
-    <img/>
+    </div>
+    <img src={pic} alt="pic of restaurant"/>
 </section>
         </>
     )
